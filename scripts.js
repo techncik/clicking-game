c54ac39784fea78df6fd21ff729d7d8a12555346
techncik <nboylan4@gmail.com>
2025-03-13 19:40:04 +1100

    Feat: Added logic to swap between start screen, game screen, and end screen. No styling or elements for these yet

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
 /* Scripts for buttongame.html */
 
+let titleDiv = document.getElementById('title-screen');
+let gameDiv = document.getElementById('game-screen');
+let endScreen = document.getElementById('end-screen');
+let display = 0;
 
+/* Starts game, swapping from title screen to game screen */
+function startGame() { 
+    titleDiv.style.display = 'none';
+    gameDiv.style.display = 'block';
+    endScreen.style.display = 'none';
+}
+
+function endGame() {
+    gameDiv.style.display = 'none';
+    endScreen.style.display = 'block';
+}
